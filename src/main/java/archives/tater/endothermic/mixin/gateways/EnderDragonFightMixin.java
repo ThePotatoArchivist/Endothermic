3c5ecc920e8dbaa53e10832b2d0c71a1494e5262
ThePotatoArchivist <josiahfu@gmail.com>
2025-09-20 13:47:05 -0700

    Generate all gateways

--- src/main/java/archives/tater/endothermic/mixin/gateways/EnderDragonFightMixin.java
+++ src/main/java/archives/tater/endothermic/mixin/gateways/EnderDragonFightMixin.java
@@ -1,6 +1,9 @@
 package archives.tater.endothermic.mixin.gateways;
 
 import archives.tater.endothermic.registry.EndothermicDataAttachments;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.boss.dragon.EnderDragonFight;
 import net.minecraft.entity.boss.dragon.EnderDragonSpawnState;
@@ -20,6 +23,10 @@ public class EnderDragonFightMixin {
     @Final
     private ServerWorld world;
 
+    @Shadow
+    @Final
+    private ObjectArrayList<Integer> gateways;
+
     @Inject(
             method = "dragonKilled",
             at = @At(value = "FIELD", target = "Lnet/minecraft/entity/boss/dragon/EnderDragonFight;dragonKilled:Z")
@@ -34,5 +41,15 @@ public class EnderDragonFightMixin {
     )
     private void updateGatewaysOpen(EnderDragonSpawnState spawnState, CallbackInfo ci) {
         world.removeAttached(EndothermicDataAttachments.GATEWAYS_OPEN);
+    }
+
+    @WrapOperation(
+            method = "dragonKilled",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/boss/dragon/EnderDragonFight;generateNewEndGateway()V")
+    )
+    private void generateAllGateways(EnderDragonFight instance, Operation<Void> original) {
+        var count = gateways.size();
+        for (var i = 0; i < count; i++)
+            original.call(instance);
     }
 }
